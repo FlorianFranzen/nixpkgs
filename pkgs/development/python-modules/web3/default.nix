0118fbd38254e93c552840da0d81eac1f17dfbab
Florian Franzen <Florian.Franzen@gmail.com>
2024-09-17 23:44:25 +0200

    pythonPackages.web3: 6.5.0 -> 7.2.0

--- pkgs/development/python-modules/web3/default.nix
+++ pkgs/development/python-modules/web3/default.nix
@@ -7,35 +7,50 @@
   eth-abi,
   eth-account,
   eth-hash,
+  eth-tester,
   eth-typing,
   eth-utils,
+  flaky,
   hexbytes,
+  hypothesis,
   ipfshttpclient,
   jsonschema,
   lru-dict,
   protobuf,
+  pydantic,
+  pytestCheckHook,
+  pytest-asyncio_0_21,
+  pytest-mock,
+  pytest-xdist,
+  pyunormalize,
+  py-evm,
   requests,
+  setuptools,
+  types-requests,
   websockets,
 }:
 
 buildPythonPackage rec {
   pname = "web3";
-  version = "6.5.0";
-  format = "setuptools";
+  version = "7.2.0";
 
-  disabled = pythonOlder "3.7";
+  pyproject = true;
+
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "web3.py";
     rev = "v${version}";
-    hash = "sha256-RNWCZQjcse415SSNkHhMWckDcBJGFZnjisckF7gbYY8=";
+    hash = "sha256-CMzcVlQKZ/ooWyYriRb/cTBE//Dkkgkn0gu0Y4ZMSrQ=";
   };
 
   # Note: to reflect the extra_requires in main/setup.py.
   passthru.optional-dependencies = {
     ipfs = [ ipfshttpclient ];
   };
+
+  nativeBuildInputs = [ setuptools ];
 
   propagatedBuildInputs =
     [
@@ -52,16 +67,35 @@ buildPythonPackage rec {
       jsonschema
       lru-dict
       protobuf
+      pydantic
       requests
+      types-requests
       websockets
     ];
 
-  # TODO: package eth-tester required for tests
-  doCheck = false;
+  nativeCheckInputs = [
+    pytestCheckHook
+    eth-tester
+    flaky
+    hypothesis
+    pytest-asyncio_0_21
+    pytest-mock
+    pytest-xdist
+    pyunormalize
+    py-evm
+  ];
 
-  postPatch = ''
-    substituteInPlace setup.py --replace "types-protobuf==3.19.13" "types-protobuf"
-  '';
+  disabledTests = [
+    # side-effect: runs pip online check and is blocked by sandbox
+    "test_install_local_wheel"
+  ];
+
+  disabledTestPaths = [
+    # requires geth library and binaries
+    "tests/integration/go_ethereum"
+    # requires local running beacon node
+    "tests/beacon"
+  ];
 
   pythonImportsCheck = [ "web3" ];
 
