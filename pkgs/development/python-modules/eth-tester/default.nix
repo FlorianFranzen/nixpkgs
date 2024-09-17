c6341b9b3f140b6c7cf9d9569a3b5f56d2cd597a
Florian Franzen <Florian.Franzen@gmail.com>
2024-09-17 22:03:43 +0200

    pythonPackages.eth-tester: init at 0.12.0-beta.1

---
+++ pkgs/development/python-modules/eth-tester/default.nix
@@ -0,0 +1,70 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  fetchpatch2,
+  pythonOlder,
+  eth-abi,
+  eth-account,
+  eth-keys,
+  eth-utils,
+  pydantic,
+  pytestCheckHook,
+  pytest-xdist,
+  py-evm,
+  rlp,
+  semantic-version,
+  setuptools,
+}:
+
+buildPythonPackage rec {
+  pname = "eth-tester";
+  version = "0.12.0-beta.1";
+
+  pyproject = true;
+
+  disabled = pythonOlder "3.8";
+
+  src = fetchFromGitHub {
+    owner = "ethereum";
+    repo = "eth-tester";
+    rev = "v${version}";
+    hash = "sha256-+ioPVADdslAUgmsaiA8hXYWxl0oAcpOPzl/fnwq1f8M=";
+  };
+
+  patches = [
+    (fetchpatch2 {
+      # Under review: https://github.com/ethereum/eth-tester/pull/298
+      name = "fix-estimate_gas.patch";
+      url = "https://github.com/ethereum/eth-tester/commit/c05cd11d33206ba752d4a44fae57311a6933c27f.patch";
+      hash = "sha256-GbZurgrlKV8fvHKqwL6zjfYyTZ2OWMu6YXMMMboIdN0=";
+    })
+  ];
+
+  nativeBuildInputs = [ setuptools ];
+
+  propagatedBuildInputs = [
+    eth-abi
+    eth-account
+    eth-keys
+    eth-utils
+    pydantic
+    rlp
+    semantic-version
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    pytest-xdist
+    py-evm
+  ];
+
+  pythonImportsCheck = [ "eth_tester" ];
+
+  meta = with lib; {
+    description = "Tool suite for testing ethereum applications.";
+    homepage = "https://github.com/ethereum/eth-tester";
+    license = licenses.mit;
+    maintainers = [ ];
+  };
+}
