9c279cf501b5feb87ade972e30b66547ac36c6ea
Florian Franzen <Florian.Franzen@gmail.com>
2024-09-17 14:23:59 +0200

    pythonPackages.eth-abi: 4.1.0 -> 5.1.0

--- pkgs/development/python-modules/eth-abi/default.nix
+++ pkgs/development/python-modules/eth-abi/default.nix
@@ -2,32 +2,33 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  pythonOlder,
   eth-hash,
   eth-typing,
   eth-utils,
   hypothesis,
   parsimonious,
   pytestCheckHook,
-  pythonOlder,
+  pytest-xdist,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "eth-abi";
-  version = "4.1.0";
-  format = "setuptools";
-  disabled = pythonOlder "3.7";
+  version = "5.1.0";
+
+  pyproject = true;
+
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "eth-abi";
     rev = "v${version}";
-    hash = "sha256-CGAfu3Ovz2WPJOD+4W2+cOAz+wYvuIyFL333Jw66ozA=";
+    hash = "sha256-D3aGMx2oZFttwQ90ouwQbbRelCb2bvyQgvamKweX9Nw=";
   };
 
-  postPatch = ''
-    substituteInPlace setup.py \
-      --replace "parsimonious>=0.9.0,<0.10.0" "parsimonious"
-  '';
+  nativeBuildInputs = [ setuptools ];
 
   propagatedBuildInputs = [
     eth-typing
@@ -35,21 +36,15 @@ buildPythonPackage rec {
     parsimonious
   ];
 
-  # lots of: TypeError: isinstance() arg 2 must be a type or tuple of types
-  doCheck = false;
-
   nativeCheckInputs = [
     hypothesis
     pytestCheckHook
+    pytest-xdist
   ] ++ eth-hash.optional-dependencies.pycryptodome;
 
   disabledTests = [
     # boolean list representation changed
     "test_get_abi_strategy_returns_certain_strategies_for_known_type_strings"
-    # hypothesis.errors.Flaky
-    "test_base_equals_has_expected_behavior_for_parsable_types"
-    "test_has_arrlist_has_expected_behavior_for_parsable_types"
-    "test_is_base_tuple_has_expected_behavior_for_parsable_types"
   ];
 
   pythonImportsCheck = [ "eth_abi" ];
